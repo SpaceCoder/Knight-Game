55d49da000fd232d414e7da7df8d9091554d559f
Anjam Nabil Islam <Anjam@anjams-air.knology.net>
2015-12-19 20:52:55 -0600

    Initial Commit

---
+++ Knight-Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Knight-Game
+//
+//  Created by Anjam Nabil Islam on 12/19/15.
+//  Copyright © 2015 Anjam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
